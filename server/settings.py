31d9f2bae05b6c30b3e721b9389f6b439e2cda95
Xi Han <xihan94@ucla.edu>
2023-08-24 04:20:27 -0700

    Update settings.py

--- server/settings.py
+++ server/settings.py
@@ -8,7 +8,7 @@ from newid import id8
 
 logging.basicConfig(level=logging.DEBUG)
 
-URI = os.getenv("URI", "http://bering.hanxi.dev:8080")
+URI = os.getenv("URI", "https://hanxi.dev:8080")
 DEV = ("heroku" in URI) or ("dev" in URI) or URI.startswith("http:")
 BR_EXTENSION = ".br" if URI.startswith("https") else ""
 
